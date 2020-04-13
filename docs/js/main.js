bf1c4473e3612ce5e880a7b850a2ee3a336939d9
mahonya333 <mahonya333@gmail.com>
2020-04-14 00:19:59 +0300

    подключены стили libs.scss, сделана часть слайдера .reviews-slider, исправлены ошибки по подключении шрифтов woff2

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,11 +1,13 @@
 $(function(){
-
     $('.reviews-slider').slick({
         slidesToShow: 1,
         slidesToScroll: 1,
-        arrows: false,
-        dots: true
+        arrows: true,
+        dots: false,
+        fade: true,
     });
-
-  
+    $(".reviews-slider").on('afterChange', function (event, slick, currentSlide) {
+        let slideNumber = (currentSlide < 9) ? ('0'+ (currentSlide + 1)) : (currentSlide + 1);
+        $("#reviews__current-slide").text(slideNumber);
+    });
 });
